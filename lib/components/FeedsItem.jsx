e7fdee21f07b91ce24ccd54c46a608e75a450002
xavizalote <xavier.cazalot@gmail.com>
2016-05-11 12:54:53 +0200

    edit feeds from the UI (+ comments block on methods/pubs + curly with spaces code style)

--- lib/components/FeedsItem.jsx
+++ lib/components/FeedsItem.jsx
@@ -1,10 +1,21 @@
 import React, { PropTypes, Component } from 'react';
+import NovaForm from "meteor/nova:forms";
+
+import SmartContainers from "meteor/utilities:react-list-container";
+const DocumentContainer = SmartContainers.DocumentContainer;
+
+import Core from "meteor/nova:core";
+const Messages = Core.Messages;
 
 class FeedsItem extends Component {
 
   constructor(props) {
     super(props);
     this.removeFeed = this.removeFeed.bind(this);
+    this.editFeed = this.editFeed.bind(this);
+    this.state = {
+      edited: false,
+    };
   }
 
   renderCategories() {
@@ -15,41 +26,74 @@ class FeedsItem extends Component {
   }
 
   renderActions() {
+
+    ({Icon} = Telescope.components);
+
     return (
-      <div className="post-actions">
-        <a onClick={this.removeFeed}>Remove this feed</a>
+      <div className="post-stats">
+        <span className="posts-stats-item" title="Edit"><a onClick={this.editFeed}><Icon name="pencil"/><span className="sr-only">Edit</span></a></span>
+        <span className="posts-stats-item" title="Delete"><a onClick={this.removeFeed}><Icon name="close"/><span className="sr-only">Delete</span></a></span>
       </div>
     )
   }
 
+  editFeed() {
+    this.setState({edited: true});
+  }
+
   removeFeed() {
-    Meteor.call('feeds.remove', this.props.feed._id);
+    const feed = this.props.feed;
+    if (window.confirm(`Delete feed “${feed.title}”?`)) {
+      Meteor.call('feeds.deleteById', feed._id, (error, result) => {
+        Messages.flash(`Feed “${feed.title}” deleted.`, "success");
+        Events.track("feed deleted", { _id: feed._id });
+      });
+    }
   }
   
   render() {
 
     ({UsersAvatar, UsersName} = Telescope.components);
 
-    const feed = this.props.feed;
+    const { feed, currentUser } = this.props;
 
     return (
       <div className="posts-item">
-        <div className="posts-item-content">
-          
-          <h3 className="posts-item-title">
-            <a className="posts-item-title-link" href={feed.url}>{feed.title ? feed.title : "Feed not fetched yet"}</a>
-            {this.renderCategories()}
-          </h3>
 
-          <div className="feeds-item-link"><a href={feed.url}>{feed.url}</a></div>
+          {this.state.edited
+            ?
+            <DocumentContainer
+              collection={Feeds}
+              publication="feeds.single"
+              selector={{_id: feed._id}}
+              terms={{_id: feed._id}}
+              joins={Feeds.getJoins()}
+              component={NovaForm}
+              componentProps={{
+                collection: Feeds,
+                currentUser,
+                methodName: "feeds.edit",
+                successCallback: () => {
+                  Messages.flash("Feed edited.", "success");
+                  this.setState({edited: false});
+                },
+                labelFunction: fieldName => Telescope.utils.getFieldLabel(fieldName, Feeds)
+              }}
+            />
+            :
+            <div className="post-item-content">
+              <h3 className="posts-item-title">
+                <a className="posts-item-title-link" href={feed.url}>{feed.title ? feed.title : "Feed not fetched yet"}</a>
+                {this.renderCategories()}
+              </h3>
+              <div className="feeds-item-link"><a href={feed.url}>{feed.url}</a></div>
 
-          <div className="posts-item-meta">
-            {feed.user? <div className="posts-item-user"><UsersAvatar user={feed.user} size="small"/><UsersName user={feed.user}/></div> : null}
-            {this.renderActions()}
-          </div>
-
-        </div>
-      
+                <div className="posts-item-meta">
+                {feed.user? <div className="posts-item-user"><UsersAvatar user={feed.user} size="small"/><UsersName user={feed.user}/></div> : null}
+                {this.renderActions()}
+              </div>
+            </div>
+          }
       </div>
     )
   }
